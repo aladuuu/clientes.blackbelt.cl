7cd8e38a987afe1a331597931c0cf2889ea35ae1
aladuuu <55522084+aladuuu@users.noreply.github.com>
2025-10-10 19:17:36 -0300

    primero

---
+++ components/invoice_delivery/language/es_es/invoice_delivery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Invoice Delivery
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2025, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['InvoiceDelivery.!error.libxml_required'] = 'Las extensiones libxml y simplexml son necesarias para la entrega de notas de cobro.';
+$lang['InvoiceDelivery.deliverinvoices.interfax_subject'] = 'Nota de Cobro #%1$s';
+$lang['InvoiceDelivery.deliverinvoices.postalmethods_description'] = 'Nota de Cobro #%1$s';
+
---
+++ language/es_es/client_invoices.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Client Invoices
+ *
+ * @package blesta
+ * @subpackage blesta.language.es_es
+ * @copyright Copyright (c) 2025, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['ClientInvoices.index.page_title'] = 'Cliente #%1$s Notas de cobro';
+$lang['ClientInvoices.index.category_open'] = 'Abrir';
+$lang['ClientInvoices.index.category_closed'] = 'Cerrado';
+$lang['ClientInvoices.index.categorylink_make_payment'] = 'Realizar el pago';
+$lang['ClientInvoices.index.boxtitle_invoices'] = 'Notas de cobro';
+$lang['ClientInvoices.index.heading_invoice'] = 'Nota de cobro #';
+$lang['ClientInvoices.index.heading_amount'] = 'Importe';
+$lang['ClientInvoices.index.heading_paid'] = 'Pagado';
+$lang['ClientInvoices.index.heading_due'] = 'Debido';
+$lang['ClientInvoices.index.heading_dateclosed'] = 'Fecha de cierre';
+$lang['ClientInvoices.index.heading_datebilled'] = 'Fecha de notas de cobro';
+$lang['ClientInvoices.index.heading_datedue'] = 'Fecha de vencimiento';
+$lang['ClientInvoices.index.heading_options'] = 'Opciones';
+$lang['ClientInvoices.index.option_view'] = 'Ver';
+$lang['ClientInvoices.index.option_pay'] = 'Pagar';
+$lang['ClientInvoices.index.no_results'] = 'No tiene notas de cobro pendientes.';
+$lang['ClientInvoices.applied.heading_paymenttype'] = 'Tipo de pago';
+$lang['ClientInvoices.applied.heading_amount'] = 'Importe';
+$lang['ClientInvoices.applied.heading_applied'] = 'Aplicada';
+$lang['ClientInvoices.applied.heading_appliedon'] = 'Aplicado en';
+$lang['ClientInvoices.applied.no_results'] = 'Esta nota de cobro no tiene ninguna transacción aplicada.';
+$lang['ClientInvoices.view.field_pay_invoice'] = 'Pagar nota de cobro';
+$lang['ClientInvoices.view.field_download'] = 'Descargar nota de cobro';
+$lang['ClientInvoices.view.invoice_terms'] = 'Términos';
+$lang['ClientInvoices.view.invoice_notes'] = 'Notas';
+$lang['ClientInvoices.view.invoice_total'] = 'Total';
+$lang['ClientInvoices.view.invoice_tax'] = '%1$s (%2$s%%)';
+$lang['ClientInvoices.view.invoice_subtotal'] = 'Subtotal';
+$lang['ClientInvoices.view.invoice_cost'] = 'Coste';
+$lang['ClientInvoices.view.invoice_unit_price'] = 'Precio unitario';
+$lang['ClientInvoices.view.invoice_quantity'] = 'Cantidad';
+$lang['ClientInvoices.view.invoice_description'] = 'Descripción';
+$lang['ClientInvoices.view.invoice_date_due'] = 'Fecha de vencimiento:';
+$lang['ClientInvoices.view.invoice_date_billed'] = 'Fecha de notas de cobro:';
+$lang['ClientInvoices.view.invoice_client_id'] = 'ID de cliente:';
+$lang['ClientInvoices.view.invoice_bill_to'] = 'Nota de cobro a:';
+$lang['ClientInvoices.view.invoice_from'] = 'De:';
+$lang['ClientInvoices.view.invoice_paid'] = 'PAGADO';
+$lang['ClientInvoices.view.heading_proforma'] = 'Proforma%1$s';
+$lang['ClientInvoices.view.heading_invoice'] = 'Nota de cobro #%1$s';
+$lang['ClientInvoices.view.boxtitle_proforma'] = 'Proforma';
+$lang['ClientInvoices.view.boxtitle_invoice'] = 'Nota de cobro';
+$lang['ClientInvoices.view.page_title'] = 'Nota de cobro #%1$s';
+$lang['ClientInvoices.index.option_download'] = 'Descargar';
+
